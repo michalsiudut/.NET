a31eddaa891b08aadac29d3091786d50c1a54f4c
Michal <michal.siudut.ms@gmail.com>
2025-02-27 18:34:38 +0100

    Zadania - wszystkie czesci

---
+++ MultiProjectNuGetDemo/MyServices/Consolelogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyServices
+{
+    public class Consolelogger : ILoggerService
+    {
+        public void log(string message)
+        {
+            Console.WriteLine($"[LOG]: {message}");
+        }
+    }
+}
---
+++ MultiProjectNuGetDemo/MyServices/ILoggerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyServices
+{
+    public interface ILoggerService
+    {
+        void log(string message);
+    }
+}
